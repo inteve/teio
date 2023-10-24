999371000d924617deee4fff4c8d01b775f29cd8
Jan Pecha <janpecha@email.cz>
2023-10-24 21:16:28 +0200

    CodeChecker: checks whole project

--- code-checker.php
+++ code-checker.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return function (JP\CodeChecker\CheckerConfig $config) {
 	$config->setPhpVersion(new JP\CodeChecker\Version('7.4.0'));
-	$config->addPath('./src');
-	$config->addPath('./tests');
+	$config->addPath('.');
 	JP\CodeChecker\Sets\CzProjectMinimum::configure($config);
 };
